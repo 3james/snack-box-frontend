09c4c5d8f2536365c8c01153c751c89dbc4deaaa
3james <3james@naver.com>
2020-12-13 10:10:27 +0900

    path alias 설정

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
+const path = require('path');
+
 module.exports = {
     devServer: {
         proxy: 'http://localhost:8090'
+    },
+    configureWebpack: {
+        resolve: {
+            alias: {
+                '@': path.join(__dirname, 'src/')
+            }
+        }
     }
 }
