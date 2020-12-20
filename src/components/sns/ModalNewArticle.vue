4fa2e8ef9de0b984dd1826a8fe22c6a5149a0fd9
3james <3james@naver.com>
2020-12-20 13:02:38 +0900

    수정기능 추가 / 리팩토링

--- src/components/sns/ModalNewArticle.vue
+++ src/components/sns/ModalNewArticle.vue
@@ -11,10 +11,10 @@
       :body-bg-variant="bodyBgVariant"
       :body-text-variant="bodyTextVariant"
       :footer-bg-variant="footerBgVariant"
-      :footer-text-variant="footerTextVariant"
+      :footer-text-variant="footerTextVariant"      
     >
-      <div style="height: 470px">
-        <b-form @submit="onSubmit" v-if="!modalModeProp.isView">
+      <div style="height: 470px; padding: 0 10px">
+        <b-form @submit="onSubmit" v-if="modalModeProp.isCreate || modalModeProp.isModify">
           <b-form-group
             id="titleFormGroup"
             label="Title"
@@ -23,10 +23,11 @@
           >
             <b-form-input
               id="titleInput"
-              v-model="article.title"
+              v-model="editArticle.title"
               type="text"
               required
               placeholder="Enter Title"
+              maxlength="80"
             />
           </b-form-group>
           <b-form-group
@@ -37,27 +38,62 @@
           >
             <b-form-textarea
               id="artlcleTextarea"
-              v-model="article.content"
+              v-model="editArticle.content"
               type="text"
               required
               placeholder
               rows="14"
+              maxlength="3000"
             />
           </b-form-group>
         </b-form>
 
-        <div v-if="modalModeProp.isView">
-          <h4><b>{{article.title}}</b></h4>
-          <b-card-text class="small text-muted">{{article.createdDate}}</b-card-text>            
-          <b-card-text style="margin-top: 45px">{{article.content}}</b-card-text>       
+        <div
+          v-if="modalModeProp.isView"
+          style="height: 470px; overflow-y: auto"
+        >
+          <h4>
+            <b>{{ article.title }}</b>
+          </h4>
+          <b-card-text class="small text-muted" size="lg">{{
+            article.createdDate
+          }}</b-card-text>
+          <b-card-text style="margin-top: 35px">{{
+            article.content
+          }}</b-card-text>
         </div>
-
       </div>
       <div slot="modal-footer" class="w-100">
-        <b-button class="float-right" style="margin-left: 5px" type="submit" variant="primary" @click="onSave" v-if="!modalModeProp.isView && !modalModeProp.isModify">저장</b-button>
-        <b-button class="float-right" style="margin-left: 5px" type="submit" variant="primary" @click="onDelete" v-if="modalModeProp.isView || modalModeProp.isModify">삭제</b-button>
-        <b-button class="float-right" style="margin-left: 5px" type="submit" variant="primary" @click="onModify" v-if="modalModeProp.isView || modalModeProp.isModify">수정</b-button>
-        <b-button class="float-right" variant="primary" @click="modalClose">닫기</b-button>
+        <b-button
+          class="float-right"
+          style="margin-left: 5px"
+          type="submit"
+          variant="primary"
+          @click="onSave"
+          v-if="modalModeProp.isCreate"
+          >저장</b-button
+        >
+        <b-button
+          class="float-right"
+          style="margin-left: 5px"
+          type="submit"
+          variant="primary"
+          @click="onDelete"
+          v-if="modalModeProp.isView || modalModeProp.isModify"
+          >삭제</b-button
+        >
+        <b-button
+          class="float-right"
+          style="margin-left: 5px"
+          type="submit"
+          variant="primary"
+          @click="onModify"
+          v-if="modalModeProp.isView || modalModeProp.isModify"
+          >수정</b-button
+        >
+        <b-button class="float-right" variant="primary" @click="modalClose"
+          >닫기</b-button
+        >
       </div>
     </b-modal>
   </b-container>
@@ -71,14 +107,14 @@ export default {
   props: {
     visibleProp: {
       type: Boolean,
-      default: false
+      default: false,
     },
     modalModeProp: {
-      type: Object
+      type: Object,
     },
     articleProp: {
-      type: Object
-    }
+      type: Object,
+    },
   },
   data() {
     return {
@@ -89,74 +125,117 @@ export default {
       footerBgVariant: "light",
       footerTextVariant: "dark",
       visible: false,
-      modalMode: {},      
+      modalMode: {},
       article: {},
+      editArticle: {},
     };
   },
   watch: {
     visibleProp(value) {
-      this.visible = value
-    },    
+      this.visible = value;
+      this.editArticle = Object.assign({}, this.articleProp);
+    },
     modalModeProp(value) {
-      this.modalMode = value
+      this.modalMode = value;
     },
     articleProp(value) {
-      this.article = value
-    }
+      this.article = value;
+    },
   },
   methods: {
-    onSave(evt) {
-      evt.preventDefault();
+    onSave(event) {
+      event.preventDefault();
       this.onSubmit();
     },
-    onSubmit(evt) {
+    onSubmit(event) {
       var self = this;
+      if (this.editArticle.title === undefined || this.editArticle.content === undefined) {
+          alert('내용을 입력하시기 바랍니다.')
+          return;
+      }
       var confirmResult = confirm("저장 하시겠습니까?");
       if (confirmResult) {
-        let form = new FormData();
-        form.append("title", this.article.title);
-        form.append("content", this.article.content);
+        let url = "/api/article";
+        let param = {
+          title: this.editArticle.title,
+          content: this.editArticle.content,
+        };
 
         axios
-          .post("/api/article", form)
-          .then(reponse => {
+          .post(url, param)
+          .then((reponse) => {
             console.log(reponse);
             self.modalClose();
             this.$parent.getArticles();
           })
-          .catch(ex => {
+          .catch((ex) => {
             console.warn("ERROR!! : ", ex);
           });
       } else {
         return;
       }
     },
-    onModify : function(evt) {
-      evt.preventDefault();
-      this.modalModeProp.isView = false
-      this.modalModeProp.isModify = true
+    onModify: function (event) {
+      event.preventDefault();
+      if (this.modalModeProp.isView) {
+        this.modalModeProp.isCreate = false;
+        this.modalModeProp.isView = false;
+        this.modalModeProp.isModify = true;
+        return;
+      }
+
+      var self = this;
+      var confirmResult = confirm("수정 하시겠습니까?");
+      if (confirmResult) {
+        let url = "/api/article/" + this.article.articleId;
+        let param = {
+          title: this.editArticle.title,
+          content: this.editArticle.content,
+        };
+
+        axios
+          .put(url, param)
+          .then((reponse) => {
+            console.log(reponse);
+            self.modalClose();
+            this.$parent.getArticles();
+          })
+          .catch((ex) => {
+            console.warn("ERROR!! : ", ex);
+          });
+      } else {
+        return;
+      }
     },
-    onDelete: function(evt) {
-      var self = this;      
+    onDelete: function (event) {
+      var self = this;
       var confirmResult = confirm("삭제 하시겠습니까?");
       if (confirmResult) {
         axios
           .delete("/api/article/" + this.article.articleId)
-          .then(reponse => {
+          .then((reponse) => {
             console.log(reponse);
             self.modalClose();
             this.$parent.getArticles();
           })
-          .catch(ex => {
+          .catch((ex) => {
             console.warn("ERROR!! : ", ex);
           });
       } else {
         return;
       }
     },
-    modalClose: function() {
-      this.$emit('closeModal');
-    }
-  }
+    modalClose: function () {
+      this.$emit("closeModal");
+      this.visible = false;
+    },
+  },
 };
 </script>
+
+<style scoped>
+div {
+  word-wrap: break-word;
+  word-break: break-all;
+}
+</style>
--- src/components/sns/SnsMain.vue
+++ src/components/sns/SnsMain.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <b-container fluid="lg" style="max-width: 1024px">
+    <b-container fluid="lg" style="max-width: 1024px; margin-bottom: 20px">
       <ModalNewArticle
         :visibleProp="articalModalVisible"
         :articleProp="articleForProp"
@@ -9,14 +9,14 @@
       </ModalNewArticle>
 
       <b-card class="mb-1 border-0 rounded-0 text-center overflow-hidden" no-body border-variant="info">
-        <b-row no-gutters align-v="center" style="background-color: #e9e9e9;">
+        <b-row class="top-banner" no-gutters align-v="center">
           <b-col md="8">
-            <b-card-img class="rounded-0" src="https://picsum.photos/1024/370/?image=16" alt="Image"></b-card-img>
+            <b-card-img class="rounded-0" src="https://picsum.photos/1024/370/?image=71" alt="Image"></b-card-img>
           </b-col>
           <b-col md="4" >
             <b-card-body>
               <b-card-text>
-                 <h5><b>Write your life !</b></h5>
+                 <h5><b>Write Your Story !</b></h5>
               </b-card-text>
               <b-button variant="primary" @click="popupNewModal()">New Story &raquo;</b-button>              
             </b-card-body>
@@ -24,7 +24,7 @@
         </b-row>        
       </b-card> 
 
-      <b-card class="mb-1 b-card-select rounded-0" 
+      <b-card class="mb-0 b-card-select rounded-0" 
               no-body 
               border-variant="link" 
               v-for="article in articleList" 
@@ -32,11 +32,13 @@
               @click="popupDetailModal(article)">
         <b-card-body>
           <b-link style="text-decoration: none;">
-            <h5 class="linksyle">
+            <span class="linksyle list-title">
               <b>{{article.title}}</b>
-            </h5>
+            </span>
           </b-link>
-          <b-card-text class="font-gray">{{article.content.length > 40 ? article.content.substr(0, 40) + ' .........' : article.content}}</b-card-text>       
+          <b-card-text class="font-gray list-content">
+            {{(article.content !== null && article.content.length > 50) ? article.content.substr(0, 50) + ' ......' : article.content}}
+          </b-card-text>       
           <b-card-text class="small text-muted">{{article.createdDate}}</b-card-text>
         </b-card-body>
         <!-- <template #footer>
@@ -71,11 +73,12 @@ export default {
       articalModalVisible: false,
       articleForProp: {},
       modalModeForProp: {
+        isCreate: false,
         isView: false,
         isModify: false
       }
     }
-  },    
+  },     
   created() {
     this.getArticles();
   },
@@ -93,15 +96,21 @@ export default {
     },
     popupNewModal: function() {
       this.articleForProp = {};
+
+      this.modalModeForProp.isCreate = true;
       this.modalModeForProp.isView = false;
       this.modalModeForProp.isModify = false;
-      this.articalModalVisible = true;
+
+      this.articalModalVisible = !this.articalModalVisible;
     },
     popupDetailModal: function(article) {      
       this.articleForProp = article;
+
+      this.modalModeForProp.isCreate = false;
       this.modalModeForProp.isView = true;
       this.modalModeForProp.isModify = false;
-      this.articalModalVisible = true;
+
+      this.articalModalVisible = !this.articalModalVisible;
     },
     onClose: function() {
       this.articalModalVisible = false;
@@ -112,13 +121,28 @@ export default {
 
 <style scoped>
 
+.top-banner {
+  background-color: #f3f1f1;
+  /* background-color: aliceblue; */
+  /* background-color: #e9e9e9; */
+}
+
+.list-title {
+  font-size: 17px;
+}
+
+.list-content {
+  margin-top: 7px;
+}
+
 .b-card-select:hover {
   cursor: pointer;
-  border: 1px solid rgb(71, 29, 170)
+  border: 1px solid rgb(166, 165, 168)
 }
 
 .b-card-select .linksyle {
-  color: #574545;
+  color: #6d6161;
+  /* color: #574545; */
 }
 
 .b-card-select:hover .linksyle {
